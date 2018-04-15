14f24ba4302f86f0ab41185c053f106adb5673df
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-15 15:15:25 -0500

    grabs location and return time

--- App.js
+++ App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, DatePickerIOS } from 'react-native';
 
 export default class App extends React.Component {
+
   constructor() {
     super();
     this.state = {
-      location: "For a run at the Ledford Branch Trail at Bent Creek"
+      location: "For a run at the Ledford Branch Trail at Bent Creek",
+      time: new Date(),
     }
     this.handleTextChange = this.handleTextChange.bind(this);
+    this.setEndTime = this.setEndTime.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+setEndTime(time) {
+  this.setState({ time })
+}
+   
 handleTextChange(location) {
   this.setState({ location });
 }
 
 handleSubmit(event) {
   event.preventDefault();
-  console.log('button clicked!');
   console.log('Trip location:', this.state.location); 
+  console.log('Return time:', this.state.time);
 }
   
   render() {
@@ -32,6 +39,12 @@ handleSubmit(event) {
           onChangeText={this.handleTextChange}
           value={this.state.location}
         />
+        <Text>What time should you be back?</Text>
+        <DatePickerIOS
+          date={this.state.time}
+          mode='time'
+          onDateChange={this.setEndTime}
+        />
         <Button onPress={this.handleSubmit} title="OK" />
       </View>
     );
@@ -40,9 +53,6 @@ handleSubmit(event) {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
