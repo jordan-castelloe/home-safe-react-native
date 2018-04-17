ad624c5be4e1e474d27294fb45aa77b60f26752a
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-17 05:48:38 -0500

    oh my god the routing works

---
+++ screens/Contacts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Contacts extends React.Component {
+
+  render(){
+    return(
+      <View>
+        <Text>
+          This is the Contacts Screen!
+        </Text>
+      </View>
+    )
+  }
+
+}
---
+++ screens/Done.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Done extends React.Component {
+
+  render() {
+    return (
+      <View>
+        <Text>
+          This is the Done Screen!
+        </Text>
+      </View>
+    )
+  }
+
+}
