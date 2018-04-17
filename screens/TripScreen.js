d28345610f932c475f3de040a2146cce4efdf82f
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-17 07:08:20 -0500

    added route to done screen

--- screens/TripScreen.js
+++ screens/TripScreen.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class TripScreen extends React.Component {
+
+  constructor() {
+    super();
+    this.finishTrip = this.finishTrip.bind(this);
+  }
+
+  finishTrip(event) {
+    event.preventDefault();
+    this.props.navigation.navigate('Done');
+  }
 
   render() {
     return (
       <View>
         <Text>
           This is the Trip screen!
+          <Button onPress={this.finishTrip} title="OK" />
         </Text>
       </View>
     )
